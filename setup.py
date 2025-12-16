ad98a31f4379f08b83c60c3d3c681f018b6c06ff
Dr Anjit <taanjit@gmail.com>
2025-12-16 07:11:47 +0000

    Fix pyproject.toml configuration and add crashcourse

--- setup.py
+++ setup.py
@@ -1,1 +1,17 @@
+from setuptools import setup, find_packages
 
+setup(
+    name="src",
+    version="0.0.1",
+    author="Dr Anjit",
+    author_email="anjit@gmail.com",
+    description="MLOps Vehicle Insurance Prediction Project",
+    long_description="MLOps Vehicle Insurance Prediction Project",
+    long_description_content_type="text/markdown",
+    url="https://github.com/taanjit/MLOPS-Project-Vehicle-insurance",
+    project_urls={
+        "Bug Reports": "https://github.com/taanjit/MLOPS-Project-Vehicle-insurance/issues",
+        "Source": "https://github.com/taanjit/MLOPS-Project-Vehicle-insurance",
+    },
+    packages=find_packages()
+)
